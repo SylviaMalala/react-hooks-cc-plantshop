c9af1a6187330bba26a226410880c7998177733f
SylviaMalala <sylviamalala17@gmail.com>
2025-10-18 00:14:15 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,57 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
+import { useState, useEffect } from 'react';
+import Header from './Header';
+import PlantCard from './PlantCard';
+import NewPlantForm from './NewPlantForm';
+import Search from './Search';
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(res => res.json())
+      .then(data => setPlants(data));
+  }, []);
+
+  const addPlant = (newPlant) => {
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/JSON'
+      },
+      body: JSON.stringify(newPlant)
+    })
+      .then(res => res.json())
+      .then(data => setPlants([...plants, data]));
+  };
+
+  const toggleSoldOut = (id) => {
+    setPlants(plants.map(plant => 
+      plant.id === id ? { ...plant, soldOut: !plant.soldOut } : plant
+    ));
+  };
+
+  const filteredPlants = plants.filter(plant => 
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <main>
+        <NewPlantForm onAddPlant={addPlant} />
+        <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+        <ul className="cards">
+          {filteredPlants.map(plant => (
+            <PlantCard 
+              key={plant.id} 
+              plant={plant} 
+              onToggleSoldOut={toggleSoldOut}
+            />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
